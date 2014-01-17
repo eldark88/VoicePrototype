4bd807d98e4995f9ec1e2d9a20f3143666249f43
Eldar Khalyknazarov <eldarkhalyknazarov@A647700-DAL-OEM.local>
2014-01-16 20:44:24 -0600

    debuging unit test

---
+++ VoicePrototype Tests/VoicePrototype_Tests.m
@@ -0,0 +1,34 @@
+//
+//  VoicePrototype_Tests.m
+//  VoicePrototype Tests
+//
+//  Created by Eldar Khalyknazarov on 1/16/14.
+//  Copyright (c) 2014 FlowTelligent. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VoicePrototype_Tests : XCTestCase
+
+@end
+
+@implementation VoicePrototype_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
